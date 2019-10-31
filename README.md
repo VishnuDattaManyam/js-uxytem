10a104b51e24fc3501c422ae32a565b569ce58bf
VishnuDattaManyam <manyamvishnudatta@gmail.com>
2019-10-31 18:58:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-uxytem
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-uxytem)
---
+++ components/text-box/text-box.js
@@ -0,0 +1,14 @@
+class TextBox {
+  constructor(settings) {
+    this.options = {
+      html: `<div>
+              <input />
+            </div>`
+    };
+    this.settings = settings;
+  }
+
+  getUI() {
+    return 
+  }
+}
